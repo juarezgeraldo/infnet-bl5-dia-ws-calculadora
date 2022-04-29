c5c061cbad1b56239576778690e5aa62e16ae3d0
Juarez Geraldo Silva Jr <juarez.junior@grupomult.com.br>
2022-04-29 15:15:49 -0300

    Fim do desenvolvimento

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -5,12 +5,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.SeekBar
+import android.widget.TextView
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, SeekBar.OnSeekBarChangeListener {
     private lateinit var txtTotalConta: EditText
     private lateinit var txtPessoas: EditText
-    private lateinit var skGorgeta: SeekBar
+    private lateinit var skGorjeta: SeekBar
     private var formatador = NumberFormat.getCurrencyInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,20 +24,44 @@ class MainActivity : AppCompatActivity(), View.OnFocusChangeListener, SeekBar.On
         txtPessoas = this.findViewById(R.id.txtPessoas)
         txtPessoas.setOnFocusChangeListener(this)
 
-        skGorgeta = this.findViewById(R.id.skGorgeta)
-        skGorgeta.setOnFocusChangeListener(this)
+        skGorjeta = this.findViewById(R.id.skGorjeta)
+        skGorjeta.setOnSeekBarChangeListener(this)
 
     }
 
     override fun onFocusChange(p0: View?, p1: Boolean) {
+        this.atualizaDadosConta()
     }
 
     override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+        val lblPercentualGorjeta = this.findViewById<TextView>(R.id.lblPercentualGorjeta)
+//        lblPercentualGorjeta.setText(skGorjeta.progress.toString() + "%")
+        lblPercentualGorjeta.setText(p1.toString() + "%")
+        this.atualizaDadosConta()
     }
 
     override fun onStartTrackingTouch(p0: SeekBar?) {
     }
 
     override fun onStopTrackingTouch(p0: SeekBar?) {
+    }
+
+    private fun atualizaDadosConta(){
+        if (txtTotalConta.text.toString().isNotEmpty() && txtPessoas.text.toString().isNotEmpty()){
+            val valorConta = txtTotalConta.text.toString().toDouble()
+            val qtdPessoas = txtPessoas.text.toString().toInt()
+
+            val lblValorGorjeta = this.findViewById<TextView>(R.id.lblValorGorjeta)
+            val valorGorjeta = valorConta * skGorjeta.progress / 100
+            lblValorGorjeta.setText(formatador.format((valorGorjeta)))
+
+            val lblValorTotal = this.findViewById<TextView>(R.id.lblValorTotal)
+            lblValorTotal.setText(formatador.format(valorConta + valorGorjeta))
+
+            val lblValorPessoa = this.findViewById<TextView>(R.id.lblValorPessoa)
+            lblValorPessoa.setText(formatador.format((valorConta + valorGorjeta) / qtdPessoas))
+
+
+        }
     }
 }
